6a5e63f681c27af9213ec54c0fa0eac413b13574
unknown <thesoupissoup@gmail.com>
2018-07-29 23:05:06 -0700

    a little more

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var userdata = [
+{
+    name: "Ahmed",
+  photo :"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores :[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+
+    {
+    name: "Jeremiah Scanlon",
+    photo :"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores:[
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5"
+    ]
+}
+]
+
+
