970230f4a1f57738979aefffbbcb77d253889889
unknown <thesoupissoup@gmail.com>
2018-07-30 11:36:40 -0700

    json friendsData is now available in browser

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,7 +6,7 @@ var friendsData = require("../data/friendsData");
 module.exports = function(app) {
      
       app.get("/api/friends", function(req, res) {
-            res.json(userData);
+            res.json(friendsData);
 
                         //This will be used to display
                                //  JSON of all possible friends
