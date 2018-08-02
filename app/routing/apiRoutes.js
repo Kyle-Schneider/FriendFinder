ba06d346c4ecb295a0d243ea761704ae6d925d3f
unknown <thesoupissoup@gmail.com>
2018-08-02 14:10:00 -0700

    comp

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -43,11 +43,36 @@ module.exports = function(app) {
       console.log(currentFriend.name);
     };
             // compatibility logic 
-        // req.body is available since we're using the body-parser middleware
-        //friendsData.push(req.body);
-   
-      });
+        // req.body is available since we're using the body-parser middleware     
+        
+         for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
 
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+
+      // If the sum of differences is less then the differences of the current "best match"
+      if (totalDifference <= bestMatch.friendDifference) {
+        // Reset the bestMatch to be the new friend.
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    
+      console.log(userData);
+      console.log(totalDifference);
+
+    // Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+    // the database will always return that the user is the user's best friend).
+    friendsData.push(userData);
+
+    // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+    res.json(bestMatch);
+
+    console.log(bestMatch);
+  });
 
 }
 
